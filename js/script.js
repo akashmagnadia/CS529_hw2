a6ec753220719a30e65098baa4c99750c1725684
Akash <akash.magnadia@gmail.com>
2022-09-22 21:29:21 -0500

    Map is filtering fill color of the state by gender murder majority. Added New data and color scale.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,72 @@
-//Width and height
-var w = 1500;
-var h = 450;
+//width and height
+var w = screen.width;
+var h = w/3;
 
 //Define map projection
-var projection = d3.geoAlbersUsa()
+var projection = d3
+    .geoAlbersUsa()
     .translate([w/2, h/2])
     .scale([1000]);
 
 //Define path generator
-var path = d3.geoPath()
+var path = d3
+    .geoPath()
     .projection(projection);
 
 //Create SVG element
-var svg = d3.select("body")
+var svg = d3
+    .select("body")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
 
-d3.json("data/us-states.geojson", function(json) {
+var canvas = d3
+    .select("body")
+    .append("canvas")
+    .attr("width", 960)
+    .attr("height", 500);
+
+var ctx = canvas.node().getContext("2d");
+
+d3.json("data/state_gender_murder.geojson", function(json) {
     //Bind data and create one path per GeoJSON feature
-    svg.selectAll("path")
-        .data(json.features)
-        .enter()
+    svg
+        .selectAll("path")
+        .data(json.features).enter()
         .append("path")
         .attr("d", path)
-        .style("fill", "steelblue");
+        .style("fill", function (d) {
+            if (d.properties.males > d.properties.females) {
+                return "steelblue";
+            } else {
+                return "red";
+            }
+        })
+        .style('stroke', '#000000');
+
+    // d3.json("data/freq_by_state.json", function(json) {
+    //     console.log(json);
+    //     console.log(json[0].males);
+    //     d3
+    //         .select("body svg")
+    //         .selectAll("path")
+    //         .data(json).enter()
+    //         .style("fill", "red");
+    // });
 });
+
+var color_sequential = d3
+    .scaleSequential(d3.interpolate("steelblue", "red"))
+    .domain([-1, 1]);
+
+var x = d3.scaleLinear().domain([-1, 1]).range([20, 500]);
+
+d3.range(-1, 1, 0.001)
+    .forEach(function (d) {
+        console.log(d);
+        ctx.beginPath();
+        ctx.strokeStyle = color_sequential(d);
+        ctx.moveTo(x(d), 80);
+        ctx.lineTo(x(d), 100);
+        ctx.stroke();
+    });
